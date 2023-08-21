6da9eff67cba0f63904ffa5707c7b47ea6685d66
dangnguyen2311 <nguyenquanghaidang2311@gmail.com>
2023-08-21 23:52:14 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Day1_1.py

--- Day1_1.py
+++
@@ -1,9 +0,0 @@
-print("Day1")
-print("🥞🥙🥟🥩🍚🍣🧊🍵🍡🍡🍬🍬🍫🍫🧁🍩")
-print("End the Code")
-# vào cmd của Python_project ở ổ D:\, gõ jupyter notebook
-# push file theo đường link sau(31min): https://youtu.be/md8YzAPgroM?t=2304
-# commit: vào source code, gõ init commit, nhấn hộp commit, chọn push
-# tạo mới respority, sao chép link code(clone), vào cmd của Python_Projects, gõ git clone + link vừa sao chép#
-# gõ: pip install jupyter, sau đó gõ: python.exe -m pip install --upgrade pip
-# thaydổi thì chỉ có trên Pycharm
